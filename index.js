07250621d8a35b0be5ccefa8199fb3c99ca3727c
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-23 08:10:56 -0400

    hashing locations of function declarations

--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 
 var esprima = require('esprima');
 var select = require('JSONSelect');
+var crypto = require('crypto');
+
+function getHash(data) {
+  return crypto
+    .createHash('md5')
+    .update(data)
+    .digest('hex');
+}
 
 function rewrite (fn) {
   // toString() on a function converts 'function  foo (..' to 'function foo(..' so we need to do the same
   return fn.replace(/function +([^ ]+)[^\(]*\(/, 'function $1(');
 }
 
-var go = module.exports = function (js) {
+var go = module.exports = function (file) {
 
 };
 
@@ -18,17 +26,34 @@ var p = __dirname + '/test/fixtures/one-root-dec.js';
 var js = fs.readFileSync(p, 'utf8');
 var fn = require(p);
 
-var ast = esprima.parse(js, { range: true });
-var decs = select.match('.type:val("FunctionDeclaration") ~ .range', ast);
+var ast = esprima.parse(js, { range: true, loc: true });
 
-var sources = decs.map(function (range) {
-  var start = range[0];
-  var end = range[1];
+var ranges = select.match('.type:val("FunctionDeclaration") ~ .range', ast)
 
-  // parse and regenerate in order to adjust whitespace to be the same as fn.toString()
+var locs = select.match('.type:val("FunctionDeclaration") ~ .loc', ast)
+
+var hashes = {};
+var fullPath = p;
+
+ranges.forEach(function (range, idx) {
+  var start =  range[0];
+  var end   =  range[1];
+
+  var loc   =  locs[idx];
+  var locS  =  loc.start;
+  var locE  =  loc.end;
+
   var fn = js.slice(start, end - start);
-  return rewrite(fn);
-});
+  var source = rewrite(fn);
+  var hash = getHash(source);
 
-var source = sources[0];
-source === fn.toString()
+  // the exact same function could exist in multiple files, so we have to store all locations
+  if (!hashes[hash]) hashes[hash] = [];
+  hashes[hash].push({
+      file  :  fullPath
+    , start :  locS
+    , end   :  locE
+    , lines :  locE.line - locS.line
+    , range :  range
+  });
+});
--- test/fixtures/one-root-dec.js
+++ test/fixtures/one-root-dec.js
@@ -1,5 +1,4 @@
-function    foo   (a, b    , c) {
-
+function foo (a, b, c) {
   console.log('logged from inside foo');
 }
 module.exports = foo;
