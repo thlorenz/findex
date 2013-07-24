e781292d9a1d78dd412ea0f3bb4364334bc6ab35
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-24 07:59:53 -0400

    browser field and alternate entry point

---
+++ browser.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var exports = module.exports = function () {
+  throw new Error('running findex on an entire project directory only works server side, pleas try findex.file(source) instead.');
+};
+exports.file = require('./file');
--- test/file.js
+++ test/file.js
@@ -3,7 +3,7 @@
 
 var test      =  require('tape')
 var fs        =  require('fs');
-var indexFile =  require('../file')
+var indexFile =  require('..').file
 
 test('\nindexing file with one root declaration', function (t) {
   var src = fs.readFileSync(__dirname + '/fixtures/one-root-dec.js', 'utf8');
