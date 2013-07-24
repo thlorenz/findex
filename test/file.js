10d9c5428f8517bcd4266c1e7c6472eacf951187
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-23 21:13:48 -0400

    adding testling tools

---
+++ test/file.js
@@ -0,0 +1,16 @@
+'use strict';
+/*jshint asi: true */
+
+var test      =  require('tape')
+var fs        =  require('fs');
+var indexFile =  require('..').file
+
+test('\nindexing file with one root declaration', function (t) {
+  var src = fs.readFileSync(__dirname + '/fixtures/one-root-dec.js', 'utf8');
+  var foo = require('./fixtures/one-root-dec');
+
+  var index = indexFile(src);
+
+  t.ok(index.find(foo));
+  t.end();
+})
